964a442d4b3242ab6a1b3ec035391821016a8973
Syl23 <42908467+Syl23@users.noreply.github.com>
2019-03-15 13:16:36 +0100

    Add files via upload

--- main.cpp
+++ main.cpp
@@ -13,6 +13,19 @@ using namespace std;
 #define COL4 1420
 
 #define BOULESRAD 20 // rayon des boulles
+#define FPS 60
+#define SPEED 1
+
+
+
+struct NivBoul{
+    int clicTime;
+    int col;
+    int joueur;
+};
+
+
+
 
 /* nemo
 le niveau est stocké dans un tableau, chaque boule est identifiée par un temps en millisecondes ou autre(moment ou elle doit etre faite cliquer)
@@ -80,35 +93,100 @@ void graphicUpdate(float boules[NBP][NBB][COO],sf::RenderWindow screen,sf::Circl
 void updatePoses(float boules[NBP][NBB][COO], float speed){
 
     for (int x = 0; x < NBP; x++)
-	for (int y = 0; y < NBB; y++)
+	for (int y = 0; y < NBB; y++){
+
+
     if (boules[x][y][1]!=-1)
 	boules[x][y][1]=boules[x][y][1]+speed;
 
+	if (boules[x][y][1]>1080)
+	boules[x][y][0]=-1;
+	boules[x][y][1]=-1;
+
+	}
+
 }
+
+void updateBoulesLevel(float boules[NBP][NBB][COO],int time,int *index,NivBoul niveau[]){
+
+    int i = *index;
+
+
+    while(i<sizeof(niveau) && niveau[i].clicTime <= time+(1080/(FPS*SPEED/1000))){
+
+        int freeBoule = 0;
+
+        while(boules[niveau[i].joueur-1][freeBoule][0] != -1) freeBoule++;
+
+
+        if(niveau[i].col == 1);
+        boules[niveau[i].joueur-1][freeBoule][0] = COL1;
+
+        if(niveau[i].col == 2);
+        boules[niveau[i].joueur-1][freeBoule][0] = COL2;
+
+        if(niveau[i].col == 3);
+        boules[niveau[i].joueur-1][freeBoule][0] = COL3;
+
+        if(niveau[i].col == 4);
+        boules[niveau[i].joueur-1][freeBoule][0] = COL4;
+
+
+
+
+        boules[niveau[i].joueur-1][freeBoule][1] = 0;
+
+
+
+        i++;
+    }
+
+    *index = i;
+
+
+
+
+
+
+}
+
+void clickBoule(int ymin, int ymax, int player, int x, float tab[NBP][NBB][COO]) {
+    for (int i = 0; i < sizeof(tab[player]); i++) {
+        if (x > tab[player][i][0]-30 && x < tab[player][i][0]+30) {
+            if (tab[player][i][1] > ymin && tab[player][i][1] < ymax) {
+                tab[player][i][1] = 0;
+            }
+        }
+    }
+}
+
+
+
+
+
 
 int main()
 {
+
+
+    NivBoul niveau[] = {{0,1,1},{500,2,1},{1000,3,1},{1500,4,1},{500,1,2},{1000,2,2},{1500,3,2},{2000,4,2}}; // un niveau a la con pour tester
+
+    int time=0;
+    int index =0;
+
+
     // Create the main window
     sf::ContextSettings settings;
-    settings.antialiasingLevel = 8;
-    sf::RenderWindow app(sf::VideoMode(800, 1000), "SFML shapes", sf::Style::Fullscreen, settings);
+    //settings.antialiasingLevel = 8;
+    sf::RenderWindow app(sf::VideoMode(1920, 1080), "SFML shapes", sf::Style::Fullscreen, settings);
+
+    app.setFramerateLimit(FPS);
 
 	// Start the game loop
 
 	float boules[NBP][NBB][COO];
     resetBoules(boules);
 
-    boules[0][0][0] = COL1-BOULESRAD;
-    boules[0][0][1] = 0;
-
-    boules[0][1][0] = COL2-BOULESRAD;
-    boules[0][1][1] = 0;
-
-    boules[1][0][0] = COL3-BOULESRAD;
-    boules[1][0][1] = 0;
-
-    boules[1][1][0] = COL4-BOULESRAD;
-    boules[1][1][1] = 0;
 
     const string titre = "yodel.ogg";
 
@@ -117,9 +195,10 @@ int main()
     if (!music.openFromFile(titre))
         return -1;
 
+
     music.play();
 
-
+    sf::Clock clock;
 
 
 
@@ -137,6 +216,31 @@ int main()
     //touche pas trop ce qu'il y a avant ça stp
 
 
+    time = clock.getElapsedTime().asMilliseconds();
+
+    // Joueur 1
+
+    if (sf::Keyboard::isKeyPressed(sf::Keyboard::Key::A)) clickBoule(700, 1000, 0, COL1, boules);
+    if (sf::Keyboard::isKeyPressed(sf::Keyboard::Key::Z)) clickBoule(700, 1000, 0, COL2, boules);
+    if (sf::Keyboard::isKeyPressed(sf::Keyboard::Key::E)) clickBoule(700, 1000, 0, COL3, boules);
+    if (sf::Keyboard::isKeyPressed(sf::Keyboard::Key::R)) clickBoule(700, 1000, 0, COL4, boules);
+
+    // Joueur 2
+
+    if (sf::Keyboard::isKeyPressed(sf::Keyboard::Key::U)) clickBoule(700, 1000, 1, COL1, boules);
+    if (sf::Keyboard::isKeyPressed(sf::Keyboard::Key::I)) clickBoule(700, 1000, 1, COL2, boules);
+    if (sf::Keyboard::isKeyPressed(sf::Keyboard::Key::O)) clickBoule(700, 1000, 1, COL3, boules);
+    if (sf::Keyboard::isKeyPressed(sf::Keyboard::Key::P)) clickBoule(700, 1000, 1, COL4, boules);
+
+
+
+
+
+
+
+
+
+
 
 
 
@@ -150,11 +254,8 @@ int main()
     app.clear(sf::Color::Black);
 
 
-
-    updatePoses(boules,0.1);
-
-
-
+    updateBoulesLevel(boules,time,&index,niveau); // le core dupde viens de la
+    updatePoses(boules,1);
 
 
 
